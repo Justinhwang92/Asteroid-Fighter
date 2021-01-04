a4400cd2b69e545e20f5514f3ee03cd8adef2afc
Hermie Baylon <hbaylonb@gmail.com>
2021-01-04 11:50:00 -0800

    Activity_Menu_Backstory1 is Refactored (Brandon's Changes)

--- app/src/main/java/com/example/myfirstapp/Activity_Menu_Backstory1.java
+++ app/src/main/java/com/example/myfirstapp/Activity_Menu_Backstory1.java
@@ -9,20 +9,28 @@ import android.view.View;
 import android.view.WindowManager;
 
 public class Activity_Menu_Backstory1 extends AppCompatActivity {
+    /**
+     * to play audio
+     */
     private MediaPlayer myBGM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set screen view to this
         setContentView(R.layout.activity_backstory1);
+        //don't show notification bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        //initialize audio
         myBGM = MediaPlayer.create(this, R.raw.bgm_credits_loop);
         myBGM.setLooping(true);
         myBGM.start();
 
+        //onclick listener for skip (go straight to play)
         findViewById(R.id.skip).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //release the resources associated with this audio player
                 if(myBGM!=null) {
                     if(myBGM.isPlaying())
                         myBGM.stop();
@@ -30,13 +38,16 @@ public class Activity_Menu_Backstory1 extends AppCompatActivity {
                     myBGM.release();
                     myBGM=null;
                 }
+                //show main game activity
                 startActivity(new Intent(Activity_Menu_Backstory1.this, Activity_Game.class));
             }
         });
 
+        //next backstory
         findViewById(R.id.continue1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //release the resources associated with this audio player
                 if(myBGM!=null) {
                     if(myBGM.isPlaying())
                         myBGM.stop();
@@ -44,10 +55,12 @@ public class Activity_Menu_Backstory1 extends AppCompatActivity {
                     myBGM.release();
                     myBGM=null;
                 }
+                //show main game activity
                 startActivity(new Intent(Activity_Menu_Backstory1.this, Activity_Menu_Backstory2.class));
             }
         });
 
+        //back to main
         findViewById(R.id.previous1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,7 +74,6 @@ public class Activity_Menu_Backstory1 extends AppCompatActivity {
                 startActivity(new Intent(Activity_Menu_Backstory1.this, Activity_Menu_Modes.class));
             }
         });
-
     }
 }
 
