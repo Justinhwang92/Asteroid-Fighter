b307568f6148321d853e74939f4925dd32a5f450
Hermie Baylon <hbaylonb@gmail.com>
2021-01-04 19:42:59 -0800

    Game_Spaceship is refactored (Brandon's Changes)

--- app/src/main/java/com/example/myfirstapp/Game_Spaceship.java
+++ app/src/main/java/com/example/myfirstapp/Game_Spaceship.java
@@ -10,40 +10,43 @@ import android.graphics.Rect;
 
 import static com.example.myfirstapp.Game_Display.screenRatioX;
 
+/**
+ * spaceship bitmap
+ */
 public class Game_Spaceship {
 
+    //self explanatory fields
     boolean hasShot = false;
     int x, y, width, height;
-    Bitmap flight, shoot;
+    Bitmap spaceship, laser;
     private Game_Display gameDisplay;
 
     // initializes the image and size for the spaceship,
     Game_Spaceship(Game_Display gameDisplay, int screenY, Resources res) {
         this.gameDisplay = gameDisplay;
 
-        flight = BitmapFactory.decodeResource(res, R.drawable.ufo);
+        spaceship = BitmapFactory.decodeResource(res, R.drawable.ufo);
 
-        width = flight.getWidth();
-        height = flight.getHeight();
+        width = spaceship.getWidth();
+        height = spaceship.getHeight();
 
         width /= 6;
         height /= 6;
 
-        flight = Bitmap.createScaledBitmap(flight, width, height, false);
+        spaceship = Bitmap.createScaledBitmap(spaceship, width, height, false);
 
-        shoot = BitmapFactory.decodeResource(res, R.drawable.ufo);
-        shoot = Bitmap.createScaledBitmap(shoot, width, height, false);
+        laser = BitmapFactory.decodeResource(res, R.drawable.ufo);
+        laser = Bitmap.createScaledBitmap(laser, width, height, false);
 
         y = screenY / 2;
         x = (int) (64 * screenRatioX);
     }
 
-    Bitmap getFlight () {
-        return flight;
+    public Bitmap getFlight () {
+        return spaceship;
     }
 
     Rect getCollisionShape() {
         return new Rect(x, y, x + width, y + height);
-        //return new Rect(x, y - 2000, x + width, y + height);
     }
 }
